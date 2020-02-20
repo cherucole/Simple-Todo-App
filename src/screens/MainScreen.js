65fabd63a8730e1717f3e9939b6e3dfc670535a1
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-02-20 13:35:16 +0300

    styled ui of homepage

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -2,21 +2,27 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import StatusBarHeader from '../components/StatusBarHeader';
 import { useSelector } from 'react-redux';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Colors from '../components/Colors';
 
 const mainScreen = props => {
   const items = useSelector(state => state.items.items);
 
   return (
     <>
-      <StatusBarHeader name="Add Items" />
+      <StatusBarHeader name="My Planner" />
       <View style={styles.container}>
-        <Button
-          title="Add Items"
-          color="red"
-          onPress={() => {
-            props.navigation.navigate('Add');
-          }}
-        />
+        <View style={styles.button}>
+          <Ionicons name="ios-add" color="white" size={37} />
+          <Button
+            title="Add Items"
+            color="white"
+            onPress={() => {
+              props.navigation.navigate('Add');
+            }}
+          />
+        </View>
         {items.map(item => (
           <View key={item.title} style={styles.item}>
             <Text>{item.title}</Text>
@@ -47,6 +53,16 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 15,
     borderRadius: 10,
+  },
+  button: {
+    flexDirection: 'row',
+    marginTop: 20,
+    height: 36,
+    backgroundColor: Colors.primary,
+    marginBottom: 10,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    borderRadius: 20,
   },
 });
 
